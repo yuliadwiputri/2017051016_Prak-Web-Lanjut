38c932361b1301c6e8e3a308aeb43d8a19e27118
Yulia Dwi Putri <83277030+yuliadwiputri@users.noreply.github.com>
2022-09-13 20:38:06 +0700

    Pertemuan3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,6 +36,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/mahasiswa', 'MahasiswaController::index');
+$routes->get('/create', 'MahasiswaController::create');
 $routes->get('/index', 'Pages::index');
 $routes->get('(:any)', 'Pages::view/$1');
 
--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Mahasiswa;
 
 class Pages extends BaseController
 {
@@ -12,7 +13,7 @@ class Pages extends BaseController
     }
     public function view($page = 'home')
     {
-        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+        if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
             // Whoops, we don't have a page for that!
             throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
         }
@@ -23,9 +24,4 @@ class Pages extends BaseController
             . view('pages/' . $page)
             . view('templates/footer');
     }
-
-
 }
-
-
-    
